71c889e91bdacbb1156df0e12c567a34d24ef30b
NerdboyQ <princtonb@gmail.com>
2020-04-13 19:26:18 -0700

    adding gitignore file

--- hw_controls/servo_tester.py
+++ hw_controls/servo_tester.py
@@ -4,4 +4,15 @@ s = servo()
 print(vars(s))
 print("max_angle: " +str(s.max_angle))
 print("slope: " +str(s.slope))
-s.max_angle = 180
+for i in range(0,3):
+    s.steer(360)
+    print("turning left")
+
+for i in range(0,6):
+    s.steer(0)
+    print("turning right")
+for i in range(0,3):
+    s.steer(360)
+    print("Return to center")
+
+s.kill_servo()
