c80fd890449a31d377736e83abd9f9b14b0170b1
Nerdboy_Q <princtonb@gmail.com>
2020-04-18 13:48:57 -0700

    Update v2_COVID_analysis.py
    added plot annotation function

--- pandas/COVID_analysis/v2_COVID_analysis.py
+++ pandas/COVID_analysis/v2_COVID_analysis.py
@@ -15,6 +15,13 @@ import numpy as np
 from matplotlib.pyplot import figure
 from pprint import pprint
 
+def add_annotation_to_vertical_graph(plots):
+	for plot in plots:
+		for i in plot.patches:
+		    # get_width pulls left or right; get_y pushes up or down
+		    plot.text(i.get_x()+0.05, i.get_height()*1.01, str(i.get_height()), fontsize=8, color='white',rotation=45)
+		plot.set_facecolor('darkgray') 
+		
 ref_dir = 'COVID-19'
 fldr_1 = 'csse_covid_19_data/csse_covid_19_time_series'
 fldr_2 = 'csse_covid_19_data/csse_covid_19_daily_reports'
@@ -82,7 +89,8 @@ print(us_max_deaths_df.head(10))
 fig1,ax1 = plt.subplots()
 subplot1 = us_max_deaths_df.head(10).plot(ax=ax1,x='Province_State',y='Deaths',kind='bar',title='Data Source: CDC, WHO, ECDC via John Hopkins repository')
 ax1.set_ylabel('COVID Deaths')
+add_annotation_to_vertical_graph([sub_plot1])
 fig1.canvas.set_window_title('Top 10 States w/ Highest COVID Deaths')
-fig1.subplots_adjust(.25,.35,.95,.95)
+fig1.subplots_adjust(.35,.45,.95,.95)
 print(us_max_deaths_df.tail(10))
 plt.show()
