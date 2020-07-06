1253fae79866f63be117f5ab1340e57220349849
Nerdboy_Q <princtonb@gmail.com>
2020-07-06 15:13:33 -0700

    Update test_camera_sanity_check.py
    added contour lines within video test.

--- Django_Projects/Bots_Projects/picar/scripts/picar/test_camera_sanity_check.py
+++ Django_Projects/Bots_Projects/picar/scripts/picar/test_camera_sanity_check.py
@@ -22,7 +22,38 @@ while True:
     except Exception:
         _, contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         contours = sorted(contours, key=lambda x:cv2.contourArea(x), reverse = True)
-
+    ##~The loop below will find our red objects and place boxes around them, however due to the break
+    ##~command, it will stop with the first (and largest) red object detected in the frame.
+    
+    if len(contours) > 0:
+        for cnt in contours:
+            (x, y, w, h) = cv2.boundingRect(cnt)
+		
+            x_medium = int((x + x +w)/2)
+            y_medium = int((y + y + h)/2)
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
+            cv2.circle(frame,(int(x_medium),int(y_medium)), int((x+w)/16),(0,255,0),2)
+            #|cv2.rectangle(frame, (0,0), (50, 50), (0,255,0), 2)
+            obj_dimensions['min_x'] = x
+            obj_dimensions['max_x'] = x+w
+            obj_dimensions['width'] = w
+            obj_dimensions['min_y'] = y+h
+            obj_dimensions['max_y'] = y
+            obj_dimensions['height'] = h
+            obj_dimensions['area'] = w*h
+            print("-"*100)
+            print("center x - point: " +str(vid.get(3)/2))
+            frame_xcenter = cvid.get(3)/2
+            object_xcenter = x + w/2
+            steering_pos = find_and_center(steering_pos,object_xcenter,frame_xcenter,s)
+            break
+    else:
+        print("Object not detected")
+    ##~Below, just to make it easier for tracking, a vertical & horizontal line will be created that will cross
+    ##~through the center of the detected object. This line can later be used to track how close the object is 
+    ##~to the center of the frame. 
+    cv2.line(frame, (x_medium,0), (x_medium,480), (0,255,0),2)
+    cv2.line(frame, (0,y_medium), (960,y_medium), (0,255,0),2)
 
     cv2.imshow('Frame',frame)
 
