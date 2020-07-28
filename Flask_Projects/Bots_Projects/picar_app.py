91fc1b624ab51416e13a0316c064c0e0bde98197
nerdboyq <princtonb@gmail.com>
2020-07-28 02:11:23 -0700

    added Flask based webapp to compare to Django

---
+++ Flask_Projects/Bots_Projects/picar_app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, url_for
+from datetime import datetime as dt
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('main.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
