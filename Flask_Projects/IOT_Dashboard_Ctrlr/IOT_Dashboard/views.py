e5a6dc0d8428ea9fca6a5a714087739dd7ed26d6
nerdboyq <princtonb@gmail.com>
2023-06-27 16:40:53 -0700

    adding changes to include bluetooth scanning feature

--- Flask_Projects/IOT_Dashboard_Ctrlr/IOT_Dashboard/views.py
+++ Flask_Projects/IOT_Dashboard_Ctrlr/IOT_Dashboard/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, jsonify, url_for, Response, stream_with_context
 from IOT_Dashboard import app
+from IOT_Dashboard.scripts.bt_ctrlr import * 
 
 @app.route("/")
 def render_homepage():
@@ -8,3 +9,10 @@ def render_homepage():
 	"""
 
 	return render_template("MainDashboard.html")
+
+@app.route("/bt_scan")
+def scan_for_bt_devices():
+    print("[views.py] calling bt scan")
+    run_bt_scanner()
+    print("[view.py] finished bt scan")
+    return jsonify({"status":"good"})
