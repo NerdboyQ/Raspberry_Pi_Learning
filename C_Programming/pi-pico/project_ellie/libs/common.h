a952a9e7d3141a1e02a47513b80b45625ef66d2a
Nerdboy_Q <princtonb@gmail.com>
2026-07-27 19:33:21 -0700

    Add common.h header file for universal definitions

---
+++ C_Programming/pi-pico/project_ellie/libs/common.h
@@ -0,0 +1,14 @@
+#ifndef UNIVERSAL_COMMON_H
+#define UNIVERSAL_COMMON_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+#include "hardware/spi.h"
+#include "hardware/i2c.h"
+
+#endif // UNIVERSAL_COMMON_H
